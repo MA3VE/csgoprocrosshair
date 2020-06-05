ff164cf47e09bf56cff78f961752897b6d267271
maeve <mujammilkhan321@gmail.com>
2020-06-05 16:57:17 +0530

    Updates

---
+++ precache-manifest.e8fdcd33070fa899f17fbd55f172c2aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538a51977d4d4106a886a4054333eb77",
+    "url": "/csgoprocrosshair/index.html"
+  },
+  {
+    "revision": "46c43ca6689e5b7d8792",
+    "url": "/csgoprocrosshair/static/css/main.ecc70d23.chunk.css"
+  },
+  {
+    "revision": "c474a5cd11a5f17198f8",
+    "url": "/csgoprocrosshair/static/js/2.7ee8c4d0.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/csgoprocrosshair/static/js/2.7ee8c4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c43ca6689e5b7d8792",
+    "url": "/csgoprocrosshair/static/js/main.388dbd44.chunk.js"
+  },
+  {
+    "revision": "5411cf964d4c0e977e92",
+    "url": "/csgoprocrosshair/static/js/runtime-main.b1512fc3.js"
+  },
+  {
+    "revision": "046e91f77483a277a434bac9ae4885e2",
+    "url": "/csgoprocrosshair/static/media/matthew.046e91f7.png"
+  }
+]);
